c3689c5064b58c2ff1bc6e649c5e397b85cd84bb
daniel500013 <bakucraft229@gmail.com>
2022-06-13 22:29:44 +0200

    added register functions

---
+++ notepad_razor/Model/SubjectModel.cs
@@ -0,0 +1,15 @@
+﻿namespace notepad_razor.Model
+{
+    [Table("Subject")]
+    public class SubjectModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Subject { get; set; }
+    }
+}
